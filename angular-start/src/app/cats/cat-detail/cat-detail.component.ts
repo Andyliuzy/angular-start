222d4020f3f3c85e810210d6f466c9fa0a9fa6ea
Andy <andyl@pansoftware.com.au>
2020-03-06 21:56:04 +1100

    custom card detail

--- angular-start/src/app/cats/cat-detail/cat-detail.component.ts
+++ angular-start/src/app/cats/cat-detail/cat-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Cat } from '../models/cat.model';
 
 @Component({
   selector: 'app-cat-detail',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cat-detail.component.scss']
 })
 export class CatDetailComponent implements OnInit {
+  @Input() cat: Cat;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
