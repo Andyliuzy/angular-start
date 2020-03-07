1a4140925b6e0c51d0c6afc88c457dd141c6fed0
Andy <andyl@pansoftware.com.au>
2020-03-07 15:45:01 +1100

    refactor

--- angular-start/src/app/cats/models/cat.model.ts
+++ angular-start/src/app/cats/models/cat.model.ts
@@ -1,8 +1,16 @@
 export interface Cat {
   id: string;
+  breed: string;
+  gender: Gender;
   name: string;
   src: string;
   age: string;
   description: string;
   adopted: boolean;
+  fee: number;
+}
+
+export enum Gender {
+  male = 'male',
+  female = 'female'
 }
